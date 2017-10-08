74ffca945e8ecdf0a78d2bd22a6a6fa8c6474f59
中村勇哉 <nakamurayuuya@nakamurayuuya-no-MacBook-Air.local>
2017-10-08 17:57:12 +0900

    add comment edit function

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,6 +13,27 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+     @comment = Comment.find(params[:id])
+     @collection = @comment.collection
+     respond_to do |format|
+       format.js { render :edit, local: { collection: @collection}  }
+     end
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    @collection = @comment.collection
+    respond_to do |format|
+      if @comment.update(comment_params)
+        format.html{ redirect_to collection_path(@collection), notice: 'コメントを更新しました.' }
+        format.js { render :index }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
   def destroy
     @comment = Comment.find(params[:id])
     @comment.destroy
@@ -24,8 +45,7 @@ class CommentsController < ApplicationController
 
 
   private
-      # ストロングパラメーター
     def comment_params
       params.require(:comment).permit(:collection_id, :content)
     end
-  end
+end
