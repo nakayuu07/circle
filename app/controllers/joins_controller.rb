cf845abbf97ef78d860766c51a443d24632fca45
中村勇哉 <nakamurayuuya@nakamurayuuya-no-MacBook-Air.local>
2017-10-18 17:53:37 +0900

    change show design

--- app/controllers/joins_controller.rb
+++ app/controllers/joins_controller.rb
@@ -1,6 +1,6 @@
 class JoinsController < ApplicationController
    before_action :authenticate_user!
-
+   
    def create
      @join = current_user.joins.create(collection_id: params[:collection_id])
      redirect_to collection_url(@join.collection_id), notice: "#{@join.collection.user.name}さんのイベントに参加します"
