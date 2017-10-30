cc913cc7503f3d09b36ddbb11db560409dc76a59
中村勇哉 <nakamurayuuya@nakamurayuuya-no-MacBook-Air.local>
2017-10-30 13:44:51 +0900

    heroku aseets

--- app/controllers/joins_controller.rb
+++ app/controllers/joins_controller.rb
@@ -1,10 +1,11 @@
 class JoinsController < ApplicationController
    before_action :authenticate_user!
-   
+
    def create
      @join = current_user.joins.create(collection_id: params[:collection_id])
      redirect_to collection_url(@join.collection_id), notice: "#{@join.collection.user.name}さんのイベントに参加します"
    end
+   
 
     def destroy
       @join = current_user.joins.find_by(collection_id: params[:collection_id]).destroy
