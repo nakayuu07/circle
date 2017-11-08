73ff0e6d7769e0a5d2327442515f3013201524f3
中村勇哉 <nakamurayuuya@nakamurayuuya-no-MacBook-Air.local>
2017-11-08 13:38:14 +0900

    refixed S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,7 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
     config.storage = :fog
-    config.fog_provider = 'fog/aws'
+    config.fog_provider = 'fog'
     config.fog_credentials = {
         :provider              => 'AWS',
         :aws_access_key_id     => ENV['S3_ACCESS_KEY_ID'],
